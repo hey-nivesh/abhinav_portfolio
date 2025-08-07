f38defccd73004f35673501f40dfbc60e7d094f7
hey-nivesh <niveshjain213@gmail.com>
2025-08-07 18:40:23 +0530

    favicon added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout() {
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/hero_img.png" type="image/png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Schibsted+Grotesk:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet" />
